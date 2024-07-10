082b37fe0379cfc84c0d7fe718fd41b5ca34e2be
Nathan Vassell <vass1881@vandals.uidaho.edu>
2024-07-10 16:24:40 -0700

    feat: Add new projects to otherProjects array

--- src/app/data/projects.ts
+++ src/app/data/projects.ts
@@ -19,10 +19,37 @@ export const featuredProjects = [
 
 export const otherProjects = [
   {
+    title: "UIdahoBandGlasses",
+    year: "2022",
+    description:
+      "Software to control the newly redesigned University of Idaho Band Glasses",
+    link: "https://github.com/lotkey/UIdahoBandGlasses",
+  },
+  {
+    title: "UidahoBandGlasses - Design Creator",
+    year: "2022",
+    description:
+      "Software to create custom designes for the University of idaho Band glasses",
+    link: "https://github.com/nvassell/Vandal-Band-Glasses-Programme",
+  },
+  {
+    title: "MiniFactoryProject - DoMoreDesigner",
+    year: "2022",
+    description:
+      "All functions an operations for the MiniFactoryProject using DoMoreDesigner.",
+    link: "https://github.com/lotkey/MiniFactoryProject",
+  },
+  {
     title: "Vandal Voice",
     year: "2022",
     description: "C# console application for vocal inputs for Data Volley",
     link: "https://github.com/nvassell/Vandal-Voice",
+  },
+  {
+    title: "FairHomesPro - Databases Final Project",
+    year: "2022",
+    description: "Database systems final using SQL and React.js.",
+    link: "https://github.com/HZ-9000/FairHomePro",
   },
   {
     title: "383-Pong-Assignment",
